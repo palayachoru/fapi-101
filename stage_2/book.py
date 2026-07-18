a2ee2d3a3a8faba3250f047a2dd153dd173a6f6d
PC <293094880+palayachoru@users.noreply.github.com>
2026-07-18 16:36:40 +0530

    validation (stage-2) done

--- stage_2/book.py
+++ stage_2/book.py
@@ -1,13 +1,43 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
 class Book:
   id: int
   title: str
   author: str
   description: str
   rating: int
+  color: str
 
-  def __init__(self, id, title, author, description, rating):
+  def __init__(self, id, title, author, description, rating, color = 'White'):
     self.id = id
     self.title = title
     self.author = author
     self.description = description
     self.rating = rating
+    self.color = color
+
+
+
+# Model for pydantic validation
+class BookRequest(BaseModel):
+  id: int  = Field(ge=1, le=100)
+  title: str  = Field(min_length=3, max_length=10)
+  author: str  = Field(min_length=3, max_length=10)
+  description: str = Field(min_length=3, max_length=100)
+  rating: int  = Field(ge=1, le=5)
+  color: Optional[str] = Field(description='Color is optional', default=None)  # optional field
+
+  # default values populated in "Example Value" section in Swagger UI
+  model_config = {
+    "json_schema_extra": {
+      "example": {
+        "id": "ICSNN Number",
+        "title": "Book's Name",
+        "author": "XXX",
+        "description": "A one liner about the book",
+        "rating": "rate out of 5",
+        "color": "book's bind color"
+      }
+    }
+  }
--- stage_2/main.py
+++ stage_2/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Path, Query, HTTPException
 
-from book import Book
+from book import Book, BookRequest
 
 
 books = [
@@ -20,3 +20,36 @@ app = FastAPI()
 @app.get('/')
 def get_books():
   return books
+
+
+@app.get("/book/{id}")            # Path parameter validation
+def get_book_by_id(id: int = Path(gt=0, le=10)):
+  for book in books:
+    if book.id == id:
+      return book
+  raise HTTPException(status_code=404, detail="Book with ID not found")
+
+
+@app.get("/book")                # Query parameter (?) validation
+def get_book_by_rating(rating: int = Query(le=1, ge=5)):
+  rbooks = []
+
+  for book in books:
+    if book.rating == rating:
+      rbooks.append(book)
+  return rbooks
+
+
+@app.post('/add-book')
+def add_book(book: BookRequest):
+  newbook = Book(**book.model_dump())
+  books.append(newbook)
+
+
+@app.delete('/delete-book/{title}')
+def delete_book(title: str):
+  for id,book in enumerate(books):
+    if book.title == title:
+      books.pop(id)
+      return
+  raise HTTPException(status_code=404, detail="Invalid book title")
